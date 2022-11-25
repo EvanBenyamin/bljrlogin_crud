ecfed5c729bebb6e05ab9c8b469698287045a209
BENJA(-) <101829863+EvanBenyamin@users.noreply.github.com>
2022-11-25 23:15:26 +0700

    Tambah CRUD
    add CRUD mahasiswa kedalam bljrlogin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,27 +3,27 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AutoLoginGuard } from './guards/auto-login.guard';
 import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
-    {
-        path: 'home',
-        loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
-        canLoad: [AuthGuard] // Secure all child pages
-    },
-    {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
-    },
-    {
-        path: 'login',
-        loadChildren: () => import('./login/login.module').then(m =>
-            m.LoginPageModule),
-        canLoad: [AutoLoginGuard]
-    },
+  {
+    path: 'home',
+    loadChildren: () => import('./mahasiswa/mahasiswa.module').then(m => m.MahasiswaPageModule),
+    canLoad: [AuthGuard] // Secure all child pages
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m =>
+      m.LoginPageModule),
+    canLoad: [AutoLoginGuard]
+  },
 ];
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-    ],
-    exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/guards/auto-login.guard.ts
+++ src/app/guards/auto-login.guard.ts
@@ -1,22 +1,14 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Router } from '@angular/router';
-import { AuthenticationService } from '../services/authentication.service';
+import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AutoLoginGuard implements CanLoad {
-  constructor(
-    private authService: AuthenticationService,
-    private router: Router,
-  ) { }
-  canLoad(): boolean {
-    if (this.authService.getData('token') != 'undefined' &&
-      this.authService.getData('token') != null) {
-      console.log('set token: ', this.authService.getData('token'));
-      this.router.navigateByUrl('/home');
-      return true;
-    } else {
-      return true;
-    }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
   }
 }
